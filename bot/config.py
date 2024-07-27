db3dcc5cd1bcb2a2e446abd3a0cd51a6baa9de76
NareshPhnix <nareshphnix@gmail.com>
2024-07-28 01:41:14 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,7 @@ class Telegram:
     API_ID = int(env.get("API_ID", "28192191"))
     API_HASH = env.get("API_HASH", "663164abd732848a90e76e25cb9cf54a")
     OWNER_ID = int(env.get("OWNER_ID", 1676244457))
-    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
+    ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS","").split()
     BOT_USERNAME = env.get("BOT_USERNAME","https://t.me/filetolinkks")
     BOT_TOKEN = env.get("BOT_TOKEN", "7146585357:AAHEb9a0Xyl9UDuSnUpwRqNpnT726APa5JE")
     CHANNEL_ID = int(env.get("CHANNEL_ID", -1002231458694 ))
